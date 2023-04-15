a4b55df4d84ecc0eb805bf5e7143b343a2de4917
mohankrishnahb@gmail.com <mohankrishnahb@gmail.com>
2023-04-15 12:58:09 +0530

    Updated new sitemap for multi sitemap integraion

--- sitemap.js
+++ sitemap.js
@@ -12,6 +12,12 @@ SalesforceInteractions.init({
                 })
             ]
         },
+        pageTypeDefault: {
+            name: "default",
+            interaction: {
+                name: "Default Page"
+            }
+        },
         pageTypes: [
             {
                 name: "home_page",
@@ -26,36 +32,40 @@ SalesforceInteractions.init({
             },
             {
                 name: "product_detail",
-                isMatch: () => {
-                    return (
-                        (/bank-demo\/loan\//.test(window.location.href)) || 
-                        (/bank-demo\/banking\//.test(window.location.href)) || 
-                        (/bank-demo\/credit-cards\//.test(window.location.href))
-                    )
-                },
-                contentZones: [
-                    { name: "related_products_container", selector: "#related-products-container" }
-                ],
+                isMatch: () => (/bank-demo\/loan\//.test(window.location.href) || /bank-demo\/banking\//.test(window.location.href) || /bank-demo\/credit-cards\//.test(window.location.href)),
                 interaction: {
-                    name: SalesforceInteractions.CatalogObjectInteractionName.ViewCatalogObject,
+                    name: "View Product",
                     catalogObject: {
                         type: "Product",
-                        id: () => {
-                            return SalesforceInteractions.mcis.getLastPathComponentWithoutExtension(window.location.href);
-                        },
+                        id: () => SalesforceInteractions.mcis.getLastPathComponentWithoutExtension(window.location.href),
                         attributes: {
+                            sku: { id: SalesforceInteractions.mcis.getLastPathComponentWithoutExtension(window.location.href) },
                             name: SalesforceInteractions.resolvers.fromSelector("h2"),
                             description: SalesforceInteractions.resolvers.fromSelector("h2"),
                             url: SalesforceInteractions.resolvers.fromHref(),
                             price: 1,
                             inventoryCount: 1,
-                            imageUrl: SalesforceInteractions.resolvers.fromSelectorAttribute("img.d-block", "src", (url) => {return url.replace("..", "https://mohankrishnahb.github.io/bank-demo")}),
-
+                            imageUrl: SalesforceInteractions.resolvers.fromSelectorAttribute("img.d-block", "src", (url) => {return url.replace("..", "https://mohankrishnahb.github.io/bank-demo")})
                         },
                         relatedCatalogObjects: {
                             Category: window.location.pathname.replace("/bank-demo/", "").replace("/", "|").replace(".html", "")
                         }
                     }
+                },
+                contentZones: [
+                    { name: "related_products_container", selector: "#related-products-container" }
+                ]
+            },
+            {
+                name: "blog",
+                isMatch: () => new Promise((resolve, reject) => {
+                    return SalesforceInteractions.DisplayUtils.pageElementLoaded("html", "html").then(() => {
+                        resolve((/bank-demo\/calculator\//.test(window.location.href)));
+                    });
+                }),
+                // isMatch: () => (/bank-demo\/loan\//.test(window.location.href) || /bank-demo\/banking\//.test(window.location.href) || /bank-demo\/credit-cards\//.test(window.location.href)),
+                interaction: {
+                    name: "view blog"
                 }
             }
         ]
